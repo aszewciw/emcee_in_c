7401c8976e1e3bc6a7e1e1456294fd41b8a57e6e
aszewciw <aszewci@gmail.com>
2017-10-26 14:50:12 -0500

    wrong print

--- parallel/mpi_tests/step_comm_alt.c
+++ parallel/mpi_tests/step_comm_alt.c
@@ -177,13 +177,13 @@ int main( int argc, char ** argv )
     if(rank==current_rank){
       fprintf(stderr, "\n");
       fprintf(stderr, "Rank %d reporting for duty:\n", rank);
-      for(int iwalker=0; iwalker<nwalkers; iwalker++){
-        int acc = my_ensemble->walker[iwalker].accept;
-        int prob = my_ensemble->walker[iwalker].lnprob;
+      for(int iwalker=0; iwalker<slice_length; iwalker++){
+        int acc = my_walkers[iwalker].accept;
+        int prob = my_walkers[iwalker].lnprob;
         fprintf(stderr, "\tWalker %d - accept %d, lnprob %lf\n", iwalker,acc,prob);
         fprintf(stderr, "\t\tParams:");
         for(int ipar=0;ipar<npars;ipar++){
-          fprintf(stderr, "\t%lf", my_ensemble->walker[iwalker].pars[ipar]);
+          fprintf(stderr, "\t%lf", my_walkers[iwalker].pars[ipar]);
         }
         fprintf(stderr, "\n");
       }
