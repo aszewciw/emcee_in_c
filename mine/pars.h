124743c5cdb50d0585ba905b743566ba6fe06b00
aszewciw <aszewci@gmail.com>
2017-11-08 18:39:51 -0600

    errors, reducing pars for debugging

--- mine/pars.h
+++ mine/pars.h
@@ -2,8 +2,8 @@
 #ifndef _PARS_HEADER_GUARD
 #define _PARS_HEADER_GUARD
 
-#define NPARS 34
+#define NPARS 2
 #define NSTEPS 5
-#define NWALKERS 1062
+#define NWALKERS 10
 
 #endif // #ifndef _PARS_HEADER_GUARD
