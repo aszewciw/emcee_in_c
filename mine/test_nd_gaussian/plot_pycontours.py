c6c6bca32171ced5784eea55d05c75130f34811c
aszewciw <aszewci@gmail.com>
2017-11-15 15:36:13 -0600

    pycontours

---
+++ mine/test_nd_gaussian/plot_pycontours.py
@@ -0,0 +1,34 @@
+'''
+code taken from http://dfm.io/emcee/current/user/quickstart/
+'''
+
+data_fname = 'means.dat'
+icov_fname = 'icov.dat'
+guesses_fname = 'guesses.dat'
+chain_fname = 'nd_gaussian_chain_pyversion.dat'
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import corner
+
+def main():
+
+    steps = np.genfromtxt(chain_fname)
+    truths = np.genfromtxt(data_fname)
+
+    signif_levels = np.array([1.0,2.0,3.0])
+    levels = 1.0 - np.exp(-0.5*signif_levels**2)
+    quantiles = [0.16,0.84]
+
+    plt.clf()
+    plt.figure(1)
+    plt_name = 'nd_gaussian_chain_pyversion_contours.png'
+    fig = corner.corner(steps,levels=levels,color='r',quantiles=quantiles,
+                        plot_density=False,plot_datapoints=False,truths=truths,
+                        fill_contours=True)
+    plt.savefig(plt_name)
+
+if __name__ == '__main__':
+    main()
