e691bcc5ca25365be46d2cda117285bb8f6ddf13
aszewciw <aszewci@gmail.com>
2017-11-15 13:47:20 -0600

    back to 100

--- mine/test_const/test.c
+++ mine/test_const/test.c
@@ -29,7 +29,7 @@ double lnprob(const double *pars, int npars, const void *userdata)
 
 int main( int argc, char ** argv )
 {
-    int ndata=10;
+    int ndata=100;
     int i, npars, nwalkers;
     double a=2.0;
 
