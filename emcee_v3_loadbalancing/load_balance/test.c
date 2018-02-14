82e752206646ba6a13c2787376d3716f7ac5fc9b
aszewciw <aszewci@gmail.com>
2018-02-14 17:44:43 -0600

    load balance test

---
+++ emcee_v3_loadbalancing/load_balance/test.c
@@ -0,0 +1,88 @@
+#include <mpi.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <float.h>
+#include <math.h>
+#include <string.h>
+
+#define WORKTAG     1
+#define DIETAG     2
+
+int main(int argc, char **argv)
+{
+    int myrank,ntasks;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+    if(argc!=2){
+        MPI_Finalize();
+        exit(EXIT_FAILURE);
+    }
+
+    if (myrank == 0) {
+        master(ntasks);
+    } else {
+        slave(myrank);
+    }
+    MPI_Finalize();
+}
+
+void master(int ntasks)
+{
+    int nprocs, rank, work, itask, tmpres, ires;
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    int results[ntasks];
+    for(itask=0;itask<ntasks;itask++){
+        results[itask]=0;
+    }
+
+    for (rank = 1; rank < nprocs; rank++) {
+        work = itask;
+        MPI_Send(&work,1,MPI_INT,rank,WORKTAG,MPI_COMM_WORLD);
+        itask++;
+    }
+
+    ires=0;
+    while (itask<ntasks) {
+        work = itask;
+        MPI_Recv(&tmpres, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        results[ires]=tmpres;
+        ires++;
+        MPI_Send(&work, 1, MPI_INT, status.MPI_SOURCE, WORKTAG, MPI_COMM_WORLD);
+        itask++;
+    }
+/*
+* Receive results for outstanding work requests.
+*/
+    for (rank = 1; rank < nprocs; ++rank) {
+        MPI_Recv(&tmpres, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        results[ires]=tmpres;
+        ires++;
+    }
+/*
+* Tell all the slaves to exit.
+*/
+    for (rank = 1; rank < ntasks; ++rank) {
+        MPI_Send(0, 0, MPI_INT, rank, DIETAG, MPI_COMM_WORLD);
+    }
+
+    for(ires=0;ires<ntasks;ires++){
+        fprintf(stderr, "%d\n", result[ires]);
+    }
+}
+
+void slave(int myrank)
+{
+    int work,result;
+    MPI_Status status;
+    while(1) {
+        MPI_Recv(&work, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        if (status.MPI_TAG == DIETAG) {
+            return;
+        }
+        result = myrank;
+        MPI_Send(&result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+}
