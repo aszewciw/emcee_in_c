892f0c20a73085d8b4a488a3f1837282627b66a7
aszewciw <aszewci@gmail.com>
2018-02-15 15:40:22 -0600

    try send recv with walker structure

--- emcee_v3_loadbalancing/load_balance/test.c
+++ emcee_v3_loadbalancing/load_balance/test.c
@@ -6,8 +6,24 @@
 #include <math.h>
 #include <string.h>
 
-#define WORKTAG     1
-#define DIETAG     2
+#define WORKTAG 1
+#define DIETAG 2
+#define NPARS 3
+
+typedef struct walker_pos{
+    int accept;
+    double lnprob;
+    double pars[NPARS];
+} walker_pos;
+
+walker_pos* allocate_walkers(size_t nwalkers){
+    struct walker_pos *self=calloc(nwalkers,sizeof(walker_pos));
+    if (self==NULL) {
+        fprintf(stderr,"Could not allocate struct walker_pos\n");
+        exit(EXIT_FAILURE);
+    }
+    return self;
+}
 
 void master(int ntasks)
 {
@@ -15,25 +31,36 @@ void master(int ntasks)
     int nprocs, rank, work, itask, tmpres, ires;
     MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    walker_pos *my_walkers;
 
-    int results[ntasks];
+    my_walkers = allocate_walkers(1);
+
+    // double results[ntasks];
+    double lnprob[ntasks];
+    // for(itask=0;itask<ntasks;itask++){
+    //     results[itask]=0;
+    // }
     for(itask=0;itask<ntasks;itask++){
-        results[itask]=0;
+        lnprob[itask]=0;
     }
     itask=0;
     for (rank = 1; rank < nprocs; rank++) {
         work = itask;
-        MPI_Send(&work,1,MPI_INT,rank,WORKTAG,MPI_COMM_WORLD);
+        // MPI_Send(&work,1,MPI_INT,rank,WORKTAG,MPI_COMM_WORLD);
+        MPI_Send(&my_walkers[0],1,MPI_WALKER,rank,WORKTAG,MPI_COMM_WORLD);
         itask++;
     }
 
     ires=0;
     while (itask<ntasks) {
-        work = itask;
-        MPI_Recv(&tmpres, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        results[ires]=tmpres;
+        // work = itask;
+        // MPI_Recv(&tmpres, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        // results[ires]=tmpres;
+        MPI_Recv(&my_walkers[0], 1, MPI_WALKER, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        lnprob[ires]=my_walkers[0].lnprob;
         ires++;
-        MPI_Send(&work, 1, MPI_INT, status.MPI_SOURCE, WORKTAG, MPI_COMM_WORLD);
+        // MPI_Send(&work, 1, MPI_INT, status.MPI_SOURCE, WORKTAG, MPI_COMM_WORLD);
+        MPI_Send(&my_walkers[0], 1, MPI_WALKER, status.MPI_SOURCE, WORKTAG, MPI_COMM_WORLD);
         itask++;
     }
 
@@ -41,8 +68,10 @@ void master(int ntasks)
 * Receive results for outstanding work requests.
 */
     for (rank = 1; rank < nprocs; rank++) {
-        MPI_Recv(&tmpres, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        results[ires]=tmpres;
+        // MPI_Recv(&tmpres, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        // results[ires]=tmpres;
+        MPI_Recv(&my_walkers[0], 1, MPI_WALKER, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        lnprob[ires]=my_walkers[0].lnprob;
         ires++;
     }
 
@@ -50,28 +79,46 @@ void master(int ntasks)
 * Tell all the slaves to exit.
 */
     for(ires=0;ires<ntasks;ires++){
-        fprintf(stderr, "\t\tRank 0: %d\n", results[ires]);
+        // fprintf(stderr, "\t\tRank 0: %d\n", results[ires]);
+        fprintf(stderr, "lnprob %d: %lf\n", ires, lnprob[ires]);
     }
 
     for (rank = 1; rank < nprocs; rank++) {
-        MPI_Send(0, 1, MPI_INT, rank, DIETAG, MPI_COMM_WORLD);
+        // MPI_Send(0, 0, MPI_INT, rank, DIETAG, MPI_COMM_WORLD);
+        MPI_Send(0, 0, MPI_WALKER, rank, DIETAG, MPI_COMM_WORLD);
     }
+
+    free(my_walkers);
 
 }
 
 void slave(int myrank)
 {
-    int work,result;
+    // int work,result;
+    int ipar;
+    walker_pos *my_walkers, *master_walkers;
+    my_walkers = allocate_walkers(1);
+    master_walkers=allocate_walkers(1);
+
+    my_walkers.accept=myrank;
+    my_walkers.lnprob=(double)myrank;
+    for(ipar=0;ipar<NPARS;ipar++){
+        my_walkers.pars[ipar]=myrank;
+    }
     MPI_Status status;
     while(1) {
-        MPI_Recv(&work, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        fprintf(stderr, "rank %d; work %d\n", myrank,work);
+        // MPI_Recv(&work, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        // fprintf(stderr, "rank %d; work %d\n", myrank,work);
+        MPI_Recv(&master_walkers, 1, MPI_WALKER, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         if (status.MPI_TAG == DIETAG) {
             break;
         }
-        result = myrank;
-        MPI_Send(&result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        // result = myrank;
+        // MPI_Send(&result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&my_walkers, 1, MPI_WALKER, 0, 0, MPI_COMM_WORLD);
     }
+    free(my_walkers);
+    free(master_walkers);
     return;
 }
 
@@ -86,11 +133,27 @@ int main(int argc, char **argv)
     }
     ntasks=atoi(argv[1]);
 
+    /*========================================================================*/
+    /* make an MPI custom structure */
+    int npars = (int)NPARS;
+    int blocklen[3] = {1, 1, npars};
+    MPI_Datatype MPI_WALKER;
+    MPI_Datatype type[3] = { MPI_INT, MPI_DOUBLE, MPI_DOUBLE };
+    MPI_Aint disp[3];
+
+    disp[0] = offsetof(walker_pos,accept);
+    disp[1] = offsetof(walker_pos,lnprob);
+    disp[2] = offsetof(walker_pos,pars);
+    MPI_Type_create_struct(3,blocklen,disp,type,&MPI_WALKER);
+    MPI_Type_commit(&MPI_WALKER);
+
+    /*========================================================================*/
     if (myrank == 0) {
         master(ntasks);
     }
     else {
         slave(myrank);
     }
+    MPI_Type_free(&MPI_WALKER);
     MPI_Finalize();
 }
