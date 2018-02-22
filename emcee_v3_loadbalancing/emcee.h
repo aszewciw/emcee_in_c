6e02d3ba74b3349cba3712e058bf1d159053652a
aszewciw <aszewci@gmail.com>
2018-02-22 06:58:39 -0600

    small commenting

--- emcee_v3_loadbalancing/emcee.h
+++ emcee_v3_loadbalancing/emcee.h
@@ -20,31 +20,13 @@ typedef struct walker_pos{
     double pars[NPARS]; /* parameters */
 } walker_pos;
 
-// /* an ensemble of walkers */
-// typedef struct ensemble {
-//     size_t nwalkers;    /* number of walkers in ensemble */
-//     size_t npars;       /* number of pars */
-//     walker_pos *walker; /* positions of walkers in ensemble */
-// } ensemble;
-
-//  stored data for the full chain
-// typedef struct chain {
-//     size_t nsteps;          /* number of steps in chain */
-//     ensemble * ensemble_A;  /* first ensemble of walkers */
-//     ensemble * ensemble_B;  /* second ensemble of walkers */
-// } chain;
-
-/* allocate space for walkers, ensembles, chains */
+/* allocate space for walkers */
 walker_pos* allocate_walkers(size_t nwalkers);
-// ensemble* allocate_ensemble(size_t nwalkers, size_t npars);
-// chain* allocate_chain(size_t nsteps, size_t nwalkers, size_t npars);
 
-/* free space for walkers, ensembles, chains */
+/* free space for walkers */
 void free_walkers(walker_pos *w);
-// void free_ensemble(ensemble *e);
-// void free_chain(chain *c);
 
-
+/* creates a "ball" of initial guesses (walker positions) */
 walker_pos* make_guess(double *centers, double *widths, size_t nwalkers, size_t npars);
 
 /* generate random number between 0 and 1 */
@@ -56,9 +38,6 @@ double normal_rand();
 /* decide to accept or reject proposed parameters based on new and old probabilities */
 int walker_accept(double lnprob_old, double lnprob_new, size_t npars, double z);
 
-/* write every step in mcmc */
-// int write_chain(const struct chain *c, const char *fname);
-
 /* write single step in chain */
 int write_step(const char *fname, const struct walker_pos *ensemble_A,
                const struct walker_pos *ensemble_B, size_t nwalkers, size_t istep);
@@ -66,16 +45,30 @@ int write_step(const char *fname, const struct walker_pos *ensemble_A,
 /* write header info of chain */
 int write_header(const char *fname, size_t nsteps, size_t nwalkers, size_t npars);
 
-
+/* creates an ensemble of "trial" walker positions */
 void create_trials(walker_pos *trial, const struct walker_pos *walkers,
                    const struct walker_pos *comp_walkers, double *z_array,
                    size_t nwalkers, double a);
 
+/* accept or reject trial positions */
 void update_positions(walker_pos *walkers, const struct walker_pos *trial,
                       double *z_array, size_t nwalkers);
 
+/*
+  manager (process 0) is responsible for:
+    (1) storing and updating the state of the ensemble
+    (2) choosing new trial positions
+    (3) distibuting trial positions among workers (when they are ready)
+    (4) accepting/rejecting trial positions
+    (5) writing the ensemble state to a file
+ */
 void manager(walker_pos *start_pos, double a, const char *fname, int nburn);
 
+/*
+  worker (processes 1 - nprocs) is responsible for:
+    (1) accepting parameters from manager
+    (2) computing lnprob for walker positions
+ */
 void worker(const void *userdata, double (*lnprob)(const double *, size_t, const void *));
 
 /* choose a random integer between 0 and n */
@@ -84,10 +77,7 @@ size_t rand_walker(size_t n);
 /* get a random z for walker step from distribution g(z) */
 double rand_gofz(double a);
 
-// void run_chain_standard(int *argc, char ***argv, walker_pos *start_pos, double a,
-//                         double (*lnprob)(const double *, size_t, const void *),
-//                         const void *userdata, const char *fname, int nburn);
-
+/* run chain with loadbalancing enabled */
 void run_chain_loadbalancing(int *argc, char ***argv, walker_pos *start_pos, double a,
                              double (*lnprob)(const double *, size_t, const void *),
                              const void *userdata, const char *fname, int nburn);
